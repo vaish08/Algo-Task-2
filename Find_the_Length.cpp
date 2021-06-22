9e06c36b44c507ea8b964d01a92d9b709600db2a
vaish08 <81669927+vaish08@users.noreply.github.com>
2021-06-22 17:51:59 +0530

    Add files via upload

---
+++ Find_the_Length.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n, q;
+    int sum = 0, res = INT_MIN;
+    int b[100005], l[100005], r[100005], v[100005];
+    std :: fill_n(b, 100005, 0);
+    cin >> n >> q;
+    for(int i = 0; i < q; i++){
+        cin >> l[i] >> r[i] >> v[i];
+    }
+    for(int i = 0; i < q; i++){
+        b[l[i]] += v[i];
+        b[r[i] + 1] -= v[i];
+    }
+
+    sum = b[1];
+    b[1] += 1;
+    for(int i = 2; i <= n; i++){
+        b[i] += sum;
+        sum = b[i];
+        b[i] += i;
+        if(res < b[i]){
+            res = b[i];
+        }
+    }
+
+    cout << res << endl;
+}
